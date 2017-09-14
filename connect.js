f5854f7960c0cb71ceef44608aa2395f70b4bcfb
hamerjjh <hamerjjh2@gmail.com$ git config --get user.namegit config --get user.namegit config --global user.email hamerjjh2@gmail.com>
2017-09-14 15:47:42 -0400

    2nd commit

--- connect.js
+++ connect.js
@@ -1,13 +1,25 @@
-const board; 
-
-cosnt settings ={
+const board = {
     xAxis: 7,
     yAxis: 6,
     piecesToConnectForWin: 4
 };
-
+const playerOne = {
+    color: "red",
+};
+const playerTwo = {
+    color: "black",
+};
 const disc = {
     none: 0,
     playerOne: 1,
     playerTwo: 2,
 };
+function changeColor() {
+    $('#A1').css("backgroundColor", "red");
+    console.log("test");
+};
+
+$('A1').click(function(){
+    $(this).css('backgroundColor', 'red');
+});
+$('#A1').on("click",changeColor)
